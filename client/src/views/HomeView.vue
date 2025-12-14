3f3aac9c9764c85362771b3aa8c92a8f235cf7f7
NeleTaisi <nele.taisi@gmail.com>
2025-12-14 18:51:28 +0200

    Added buttons to home page

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,21 +1,43 @@
 <template>
   <div id="content">
+    
+
     <div id="content-positioner">
       <div class="sidebar left"></div>
-        <div id="feed-of-posts">
-          <!-- Render posts from store -->
-          <PostItem
-            v-for="post in allPosts" 
-            :key="post.id" 
-            :post="post" 
-          />
 
-          <div class="reset-likes-container">
-            <button @click="resetAllLikes" class="reset-likes-btn">
-              Reset All Likes
+        <div class="center-column">
+          <div class="logout-bar">
+            <button class="logout-btn" @click="logout">
+              Logout
+            </button>
+          </div>
+
+          <div id="feed-of-posts">
+            <!-- Render posts from store -->
+            <PostItem
+              v-for="post in allPosts" 
+              :key="post.id" 
+              :post="post" 
+            />
+
+            <div class="reset-likes-container">
+              <button @click="resetAllLikes" class="reset-likes-btn">
+                Reset All Likes
+              </button>
+            </div>
+          </div>
+
+          <div class="home-actions">
+            <button class="action-btn add" @click="$router.push('/add-post')">
+              Add Post
+            </button>
+
+            <button class="action-btn delete">
+              Delete All
             </button>
           </div>
         </div>
+
       <div class="sidebar right"></div>
     </div>
   </div>
@@ -43,6 +65,7 @@ export default {
     resetAllLikes() {
       this.resetLikes();
     }
+    
   }
 }
 
